56f201f3411630704d67adab84ead01b93f14c56
Ilias Bougoudis <ibougoudis@yahoo.gr>
2026-07-13 02:09:23 -0700

    File that runs the model and produces the predictions

---
+++ src/api/predictor.py
@@ -0,0 +1,19 @@
+
+"""
+Prediction helper functions - converting the JSON to numpy and calling sklearn.
+"""
+
+from .model_info import load_model_info
+from .model_loader import model
+
+info = load_model_info()
+
+def predict(features):
+
+    expected = (info['drivers'] + info['spatial'] + info['day_input'])
+
+    X = [[features[name] for name in expected]]
+
+    predictions = model.predict(X)
+
+    return predictions.tolist() # for proper FastAPI serialization 
