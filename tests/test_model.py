74556d46defa0d2e9eac45eb88caea44256e57c7
Ilias Bougoudis <ibougoudis@yahoo.gr>
2026-07-13 02:06:00 -0700

    Included a test for the deployment model

--- tests/test_model.py
+++ tests/test_model.py
@@ -43,6 +43,27 @@ def test_save_load_model(tmp_path):
 
     assert np.array_equal(predictions, loaded_predictions) # if the predictions of the 2 models are the same.
 
+def test_save_load_model_dep(tmp_path):
+
+    # Creating dummy fake inputs
+    X = np.random.rand(10, 5)
+    y = np.random.rand(10)
+
+    # Creating the model and training it.
+    model = Diatom_pr_regressor(n_bins=5, drivers_idx=[0, 1], spatial_idx=[2, 3], day_idx=[4])
+    model.train(X, y)
+    predictions = model.model.predict(X)
+
+    # Saving the model.
+    output.save_api_model(str(tmp_path), 'tmp_model.joblib', model)
+
+    # Loading the model.
+    loaded_model = output.load_api_model(str(tmp_path), 'tmp_model.joblib')
+
+    loaded_predictions = loaded_model.predict(X)
+
+    assert np.array_equal(predictions, loaded_predictions) # if the predictions of the 2 models are the same.
+
     
 
 
